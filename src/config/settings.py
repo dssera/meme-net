06f2be7e12e119b024a5635b5069f4a74844f0ff
dssera <adesser001@mail.ru>
2024-07-06 10:56:50 +0300

    config apps

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth_core.apps.AuthCoreConfig',
+    'blog_core.apps.BlogCoreConfig',
 ]
 
 MIDDLEWARE = [
